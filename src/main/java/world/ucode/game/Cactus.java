4f96ce877be80a214edc589e255a6bb50c651001
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-09-21 17:56:32 +0300

    Add files

---
+++ src/main/java/world/ucode/game/Cactus.java
@@ -0,0 +1,93 @@
+package world.ucode.game;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+
+public class Cactus {
+    protected Pane root;
+    protected ImageView cactus;
+    protected Image cactusOne = new Image("Cactus-1.png");
+    protected Image cactusTwo = new Image("Cactus-2.png");
+    protected Image cactusTree = new Image("Cactus-3.png");
+    protected Image cactusFore = new Image("Cactus-4.png");
+    protected Image cactusFive = new Image("Cactus-5.png");
+    protected ArrayList<Image> listImage = new ArrayList<Image>();
+    protected ArrayList<ImageView> listImageView = new ArrayList<ImageView>();
+
+    /**
+     * Constructor Cactus
+     *
+     * @param pane
+     */
+    Cactus(Pane pane) {
+        this.root = pane;
+        this.setListImage();
+        this.setPositionImg();
+        this.animation().start();
+
+    }
+
+    /**
+     * Set ListImage
+     */
+    protected void setListImage() {
+        listImage.add(cactusOne);
+        listImage.add(cactusTwo);
+        listImage.add(cactusTree);
+        listImage.add(cactusFore);
+        listImage.add(cactusFive);
+    }
+
+    /**
+     * Set PositionImg
+     */
+    protected void setPositionImg() {
+        for (int i = 0; i < listImage.size(); i++) {
+            cactus = new ImageView();
+            cactus.setImage(listImage.get(i));
+            cactus.setLayoutY(720);
+            cactus.setLayoutX(random());
+            listImageView.add(cactus);
+        }
+        root.getChildren().addAll(listImageView);
+    }
+
+    /**
+     * Random cactus
+     *
+     * @return double
+     */
+    private double random() {
+        double result = 1200 + (int) (Math.random() * 15) * 100;
+
+        for (ImageView y : listImageView) {
+            if (Math.abs(result - y.getLayoutX()) < 250)
+                result = -50;
+        }
+        return result;
+    }
+
+    /**
+     * Animation cactus
+     *
+     * @return AnimationTimer
+     */
+    protected AnimationTimer animation() {
+        AnimationTimer animation = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                for (ImageView i : listImageView) {
+                    i.setLayoutX(i.getLayoutX() - 3);
+                    if (i.getLayoutX() < -50) {
+                        i.setLayoutX(random());
+                    }
+                }
+            }
+        };
+        return animation;
+    }
+}
--- src/main/java/world/ucode/game/Game.java
+++ src/main/java/world/ucode/game/Game.java
@@ -1,5 +1,8 @@
 package world.ucode.game;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import world.ucode.window.GameWindow;
@@ -7,23 +10,32 @@ import world.ucode.window.GameWindow;
 public class Game {
     protected Pane root = new Pane();
     protected Stage primaryStage;
-
+    public Dino dino = new Dino(this.root);
     /**
      * @param primaryStage
      */
     Game(Stage primaryStage) {
         this.primaryStage = primaryStage;
-    }
-
-    public void start() {
-        primaryStage.setScene(new GameWindow(this.root).create());
         new Sun(root);
         new Cloud(root);
         new Score(root);
         new Ground(root);
-        new Dino(root);
+        new Cactus(root);
+        this.event();
+    }
+    public void start() {
+        primaryStage.setScene(new GameWindow(this.root).create());
+        event();
         primaryStage.centerOnScreen();
         root.setStyle("-fx-background-color: linear-gradient(to bottom, #C0C0C0, #ffffff);");
     }
 
+    private void event() {
+        primaryStage.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent key) {
+                if (key.getCode() == KeyCode.SPACE)
+                    dino.animationTimer.start();
+            }
+        });
+    }
 }
